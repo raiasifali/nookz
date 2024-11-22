99c96efd1ad79c52d98a8cd8cfb3e399e82919ea
raiasifali <asif.developer92@gmail.com>
2024-11-22 20:38:49 +0500

    Add middleware

--- next.config.ts
+++ next.config.ts
@@ -2,34 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["c.animaapp.com"], // Add the external domain here
-  },
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'www.nookz.co.uk',
-          },
-        ],
-        destination: 'https://nookz.co.uk/:path*',
-        permanent: true,
-      },
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'protocol',
-            value: 'http',
-          },
-        ],
-        destination: 'https://nookz.co.uk/:path*',
-        permanent: true,
-      },
-    ];
-  },
+    domains: ["c.animaapp.com"],
+  }
 };
 
 export default nextConfig;
